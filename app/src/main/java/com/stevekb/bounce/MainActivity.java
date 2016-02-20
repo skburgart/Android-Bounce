0374dfd896c3b5b8a12b35c5c34251d177e7fdcf
Steven Burgart <skburgart@gmail.com>
2016-02-19 23:36:46 -0500

    Upgrade to android 23

--- app/src/main/java/com/stevekb/bounce/MainActivity.java
+++ app/src/main/java/com/stevekb/bounce/MainActivity.java
@@ -11,6 +11,9 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 public class MainActivity extends Activity implements SensorEventListener {
 
     private SensorManager mSensorManager;
@@ -21,6 +24,10 @@ public class MainActivity extends Activity implements SensorEventListener {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().addTestDevice("5DA56B53019C0A99B3AF21C923250D0F").build();
+        mAdView.loadAd(adRequest);
 
         mySurface = (PhysicsSurface) findViewById(R.id.mySurface);
 
